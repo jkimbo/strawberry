de4472f6079e35b20fc78615e04811a8bd58ffdf
Patrick Arminio <patrick.arminio@gmail.com>
2020-07-29 10:15:01 +0100

    Add devcontainer

--- strawberry/types/types.py
+++ strawberry/types/types.py
@@ -40,8 +40,6 @@ class TypeDefinition:
             fields = _get_fields(self.origin)
             self._fields = _resolve_types(fields)
 
-        # type_params =
-
         return self._fields
 
     @property
